e8ec8b61376557fb0048c95334d09b7510f8748f
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-08-28 09:12:58 -0300

    Creating Callback example

--- callback/sync.js
+++ callback/sync.js
@@ -1,5 +1,10 @@
 var fs = require("fs");
 
+/*
+	sync - blocking
+	This example example shows that the program blocks until it reads
+	the file and then only it proceeds to end the program.
+*/
 var data = fs.readFileSync('input.txt');
 
 console.log(data.toString());
